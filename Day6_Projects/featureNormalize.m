c0d2e81ef1f69cc3944e2c081418ebff49c6c4d3
Kritanjali Jain <kritanjalijain@gmail.com>
2020-09-07 20:33:05 +0530

    Update featureNormalize.m

--- Day6_Projects/featureNormalize.m
+++ Day6_Projects/featureNormalize.m
@@ -4,7 +4,7 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-
+% ============================================================
 
 mu = mean(X);
 sigma = std(X);

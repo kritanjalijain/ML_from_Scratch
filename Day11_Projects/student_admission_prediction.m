f7d683d761945fc43ce2e2f5e5dbe4b438a800e3
Kritanjali Jain <kritanjalijain@gmail.com>
2020-09-12 22:32:38 +0530

    Create student_admission_prediction.m

---
+++ Day11_Projects/student_admission_prediction.m
@@ -0,0 +1,120 @@
+clear
+dir
+
+% Load Data
+% The first two columns contain the exam scores and the third column contains the label.
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); 
+y = data(:, 3);
+
+% Plot the data with + indicating (y = 1) examples and o indicating (y = 0) examples.
+plotData(X, y);
+ 
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+
+% Provide input values to the sigmoid function below and run to check your implementation
+sigmoid(0)
+
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display the initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+disp('Gradient at initial theta (zeros):'); 
+disp(grad);
+
+%  Set options for fminunc
+options = optimoptions(@fminunc,'Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 400);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+disp('theta:');disp(theta);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+% Add some labels 
+hold on;
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+hold off;
+
+%  Predict probability for a student with score 45 on exam 1  and score 85 on exam 2 
+prob = sigmoid([1 45 85] * theta);
+fprintf('For a student with scores 45 and 85, we predict an admission probability of %f\n\n', prob);
+% Compute accuracy on our training set
+p = predict(theta, X);
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+
+%  The first two columns contains the X values and the third column
+%  contains the label (y).
+data = load('ex2data2.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+plotData(X, y);
+% Put some labels 
+hold on;
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+% Specified in plot order
+legend('y = 1', 'y = 0')
+hold off;
+
+% Add Polynomial Features
+% Note that mapFeature also adds a column of ones for us, so the intercept term is handled
+X = mapFeature(X(:,1), X(:,2));
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 1;
+
+% Compute and display initial cost and gradient for regularized logistic regression
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+lambda = 100;
+% Set Options
+options = optimoptions(@fminunc,'Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 1000);
+
+% Optimize
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+hold on;
+title(sprintf('lambda = %g', lambda))
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+legend('y = 1', 'y = 0', 'Decision boundary')
+hold off;
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);

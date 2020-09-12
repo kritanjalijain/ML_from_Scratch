16774d953224a15924dac5123037cdd8e196b37a
Kritanjali Jain <kritanjalijain@gmail.com>
2020-09-12 22:40:55 +0530

    Update student_admission_prediction.m

--- Day11_Projects/student_admission_prediction.m
+++ Day11_Projects/student_admission_prediction.m
@@ -3,7 +3,7 @@ dir
 
 % Load Data
 % The first two columns contain the exam scores and the third column contains the label.
-data = load('ex2data1.txt');
+data = load('studentdata1.txt');
 X = data(:, [1, 2]); 
 y = data(:, 3);
 

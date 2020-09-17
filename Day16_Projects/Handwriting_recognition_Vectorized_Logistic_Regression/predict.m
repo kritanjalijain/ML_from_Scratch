01ba49c50c73d98713afc8ef50ad5b5e15f09975
Kritanjali Jain <kritanjalijain@gmail.com>
2020-09-17 20:10:44 +0530

    Update predict.m

--- Day16_Projects/Handwriting_recognition_Vectorized_Logistic_Regression/predict.m
+++ Day16_Projects/Handwriting_recognition_Vectorized_Logistic_Regression/predict.m
@@ -6,9 +6,8 @@ num_labels = size(Theta2, 1);
 
 p = zeros(size(X, 1), 1);
 
-a1 = [ones(m,1) X]; % 5000 x 401 == no_of_input_images x no_of_features % Adding 1 in X 
-%No. of rows = no. of input images
-%No. of Column = No. of features in each image
+a1 = [ones(m,1) X];
+
   
 z2 = a1 * Theta1';  % 5000 x 25
 a2 = sigmoid(z2);   % 5000 x 25
@@ -19,11 +18,7 @@ z3 = a2 * Theta2';  % 5000 x 10
 a3 = sigmoid(z3);  % 5000 x 10
   
 [prob, p] = max(a3,[],2); 
-%returns maximum element in each row  == max. probability and its index for each input image
-%p: predicted output (index)
-%prob: probability of predicted output
 
 % =========================================================================
-
 
 end

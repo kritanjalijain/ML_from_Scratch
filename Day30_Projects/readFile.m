16e737f88eb36c53a4ab7ed7d302d86dccac7d2e
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-06 19:53:36 +0530

    Update readFile.m

--- Day30_Projects/readFile.m
+++ Day30_Projects/readFile.m
@@ -1,8 +1,5 @@
 function file_contents = readFile(filename)
 %READFILE reads a file and returns its entire contents 
-%   file_contents = READFILE(filename) reads a file and returns its entire
-%   contents in file_contents
-%
 
 % Load File
 fid = fopen(filename);

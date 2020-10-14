9c44e42687a2521a095df042df7bbfa4ccb684af
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-14 15:22:12 +0530

    Update runkMeans.m

--- Day40_Projects/runkMeans.m
+++ Day40_Projects/runkMeans.m
@@ -2,23 +2,14 @@ function [centroids, idx] = runkMeans(X, initial_centroids, ...
                                       max_iters, plot_progress)
 %RUNKMEANS runs the K-Means algorithm on data matrix X, where each row of X
 %is a single example
-%   [centroids, idx] = RUNKMEANS(X, initial_centroids, max_iters, ...
-%   plot_progress) runs the K-Means algorithm on data matrix X, where each 
-%   row of X is a single example. It uses initial_centroids used as the
-%   initial centroids. max_iters specifies the total number of interactions 
-%   of K-Means to execute. plot_progress is a true/false flag that 
-%   indicates if the function should also plot its progress as the 
-%   learning happens. This is set to false by default. runkMeans returns 
-%   centroids, a Kxn matrix of the computed centroids and idx, a m x 1 
-%   vector of centroid assignments (i.e. each entry in range [1..K])
-%
+
 
 % Set default value for plot progress
 if ~exist('plot_progress', 'var') || isempty(plot_progress)
     plot_progress = false;
 end
 
-% Plot the data if we are plotting progress
+% Plot the data if plotting progress
 if plot_progress
     figure;
     hold on;
@@ -43,7 +34,7 @@ for i=1:max_iters
     % For each example in X, assign it to the closest centroid
     idx = findClosestCentroids(X, centroids);
     
-    % Optionally, plot progress here
+    % Optionally, plot progress 
     if plot_progress
         plotProgresskMeans(X, centroids, previous_centroids, idx, K, i);
         previous_centroids = centroids;
@@ -55,7 +46,7 @@ for i=1:max_iters
     centroids = computeCentroids(X, idx, K);
 end
 
-% Hold off if we are plotting progress
+% Hold off if plotting progress
 if plot_progress
     hold off;
 end

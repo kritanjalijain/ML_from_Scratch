38bd2570057722e8d4479f4426eea07c6ee3c41f
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-19 23:07:00 +0530

    Update restaurant_linear_regression_1.m

--- Day5_Projects/restaurant_linear_regression_1.m
+++ Day5_Projects/restaurant_linear_regression_1.m
@@ -1,59 +1,44 @@
-%% ======================= Part 1: Problem Statement =======================
 % Predict profits of Linear regression using batch graident descent algorithm
-%% Initialization
+
 clear ; close all; clc
 
+%% Plot Data
 
-
-%% ======================= Part 2: Plotting =======================
-fprintf('Plotting Data ...\n')
-data = load('ex1data1.txt');
+data = load('data.txt');
 X = data(:, 1); y = data(:, 2);
-m = length(y); % number of training examples
-
-% Plot Data
-% Function from plotData.m
-
+m = length(y); 
 plotData(X, y);
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program has been paused. Press enter to continue.\n');
 pause;
 
-%% =================== Part 3: Cost and Gradient descent ===================
+%% Cost and Gradient descent 
 
 X = [ones(m, 1), data(:,1)]; 
-theta = zeros(2, 1); % initialize fitting parameters
-
-% Some gradient descent settings
+theta = zeros(2, 1); 
 iterations = 1500;
 alpha = 0.01;
 
-fprintf('\nTesting the cost function ...\n')
+fprintf('\nTesting the cost function \n')
 % compute and display initial cost
 J = computeCost(X, y, theta);
 fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
-fprintf('Expected cost value (approx) 32.07\n');
 
-% further testing of the cost function
-J = computeCost(X, y, [-1 ; 2]);
+J = computeCost(X, y, [-1 ; 2]); %test
 fprintf('\nWith theta = [-1 ; 2]\nCost computed = %f\n', J);
-fprintf('Expected cost value (approx) 54.24\n');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program has been paused. Press enter to continue.\n');
 pause;
 
-fprintf('\nRunning Gradient Descent ...\n')
+fprintf('\nRunning Gradient Descent \n')
 % run gradient descent
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
-% print theta to screen
 fprintf('Theta found by gradient descent:\n');
 fprintf('%f\n', theta);
-fprintf('Expected theta values (approx)\n');
-fprintf(' -3.6303\n  1.1664\n\n');
 
 % Plot the linear fit
-hold on; % keep previous plot visible
+hold on; 
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
 hold off 
@@ -66,10 +51,10 @@ predict2 = [1, 7] * theta;
 fprintf('For population = 70,000, we predict a profit of %f\n',...
     predict2*10000);
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program has been paused. Press enter to continue.\n');
 pause;
 
-%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
+%% Visualizing J(theta_0, theta_1)
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
 % Grid over which J is calculated
@@ -79,15 +64,12 @@ theta1_vals = linspace(-1, 4, 100);
 % initialize J_vals to a matrix of 0's
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
-% Fill out J_vals
 for i = 1:length(theta0_vals)
     for j = 1:length(theta1_vals)
 	  t = [theta0_vals(i); theta1_vals(j)];
 	  J_vals(i,j) = computeCost(X, y, t);
     end
 end
-
-
 
 J_vals = J_vals';
 % Surface plot

ce2217c57016d0abeff0e1eee1c29a1a47a0fdb2
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-19 23:21:43 +0530

    Update prices_of_house_multivariant_regression.m

--- Day6_Projects/prices_of_house_multivariant_regression.m
+++ Day6_Projects/prices_of_house_multivariant_regression.m
@@ -1,27 +1,24 @@
 %% Initialization
 
-%% ================ Part 1: Feature Normalization ================
+%% Feature Normalization
 
-%% Clear and Close Figures
 clear ; close all; clc
 
-fprintf('Loading data ...\n');
-
 %% Load Data
-data = load('ex1data2.txt');
+data = load('data.txt');
 X = data(:, 1:2);
 y = data(:, 3);
 m = length(y);
 
 % Print out some data points
-fprintf('First 10 examples from the dataset: \n');
+fprintf('The first 10 examples from the dataset are: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program has been paused. Press enter to continue.\n');
 pause;
 
 % Scale features and set them to zero mean
-fprintf('Normalizing Features ...\n');
+fprintf('Normalizing Features \n');
 
 [X mu sigma] = featureNormalize(X);
 
@@ -29,15 +26,10 @@ fprintf('Normalizing Features ...\n');
 X = [ones(m, 1) X];
 
 
-%% ================ Part 2: Gradient Descent ================
+%% Gradient Descent
 
-fprintf('Running gradient descent ...\n');
-
-% Choose some alpha value
 alpha = 0.01;
 num_iters = 400;
-
-% Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -59,18 +51,15 @@ X1 = [1 X1]
 price = X1 * theta;
 % ============================================================
 
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         '(using gradient descent):\n $%f\n'], price);
+fprintf(['Predicted price of a 1650 sq-ft, 3 br house ''\n $%f\n'], price);
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program has been paused. Press enter to continue.\n');
 pause;
 
-%% ================ Part 3: Normal Equations ================
-
-fprintf('Solving with normal equations...\n');
+%% Normal Equations
 
 % Load Data
-data = csvread('ex1data2.txt');
+data = csvread('data.txt');
 X = data(:, 1:2);
 y = data(:, 3);
 m = length(y);
@@ -88,13 +77,11 @@ fprintf('\n');
 
 
 % Estimate the price of a 1650 sq-ft, 3 br house
-
-price = 0; % You should change this
+price = 0;
 X1 = [1 1650 3];
 price = X1 * theta;
 
 % ============================================================
 
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         '(using normal equations):\n $%f\n'], price);
+fprintf(['Predicted price of a 1650 sq-ft, 3 br house ''\n $%f\n'], price);
 

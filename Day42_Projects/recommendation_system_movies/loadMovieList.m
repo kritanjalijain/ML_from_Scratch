28859b9f4650301bfe66e59c339c40490d5d79df
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-29 20:20:50 +0530

    Update loadMovieList.m

--- Day42_Projects/recommendation_system_movies/loadMovieList.m
+++ Day42_Projects/recommendation_system_movies/loadMovieList.m
@@ -1,9 +1,6 @@
 function movieList = loadMovieList()
 %GETMOVIELIST reads the fixed movie list in movie.txt and returns a
 %cell array of the words
-%   movieList = GETMOVIELIST() reads the fixed movie list in movie.txt 
-%   and returns a cell array of the words in movieList.
-
 
 %% Read the fixed movieulary list
 fid = fopen('movie_ids.txt');

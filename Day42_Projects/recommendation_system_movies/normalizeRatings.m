0cdf7a38d4f7bc4188cf0cadca3e12d7843749d2
Kritanjali Jain <kritanjalijain@gmail.com>
2020-10-29 20:20:01 +0530

    Update normalizeRatings.m

--- Day42_Projects/recommendation_system_movies/normalizeRatings.m
+++ Day42_Projects/recommendation_system_movies/normalizeRatings.m
@@ -1,9 +1,6 @@
 function [Ynorm, Ymean] = normalizeRatings(Y, R)
-%NORMALIZERATINGS Preprocess data by subtracting mean rating for every 
-%movie (every row)
-%   [Ynorm, Ymean] = NORMALIZERATINGS(Y, R) normalized Y so that each movie
-%   has a rating of 0 on average, and returns the mean rating in Ymean.
-%
+%NORMALIZERATINGS Preprocess data by subtracting mean rating for every movie (every row)
+
 
 [m, n] = size(Y);
 Ymean = zeros(m, 1);

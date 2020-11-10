b6458f6d8d40eb6df34db9b5e9f9e2c09485c479
Kritanjali Jain <kritanjalijain@gmail.com>
2020-11-10 23:02:31 +0530

    Update svmTrain.m

--- Day30_Projects/Spam_Email_Classifier/svmTrain.m
+++ Day30_Projects/Spam_Email_Classifier/svmTrain.m
@@ -2,18 +2,10 @@ function [model] = svmTrain(X, Y, C, kernelFunction, ...
                             tol, max_passes)
 %SVMTRAIN Trains an SVM classifier using a simplified version of the SMO 
 %algorithm. 
-%   [model] = SVMTRAIN(X, Y, C, kernelFunction, tol, max_passes) trains an
-%   SVM classifier and returns trained model. X is the matrix of training 
-%   examples.  Each row is a training example, and the jth column holds the 
-%   jth feature.  Y is a column matrix containing 1 for positive examples 
-%   and 0 for negative examples.  C is the standard SVM regularization 
-%   parameter.  tol is a tolerance value used for determining equality of 
-%   floating point numbers. max_passes controls the number of iterations
-%   over the dataset (without changes to alpha) before the algorithm quits.
-%
-% Note: This is a simplified version of the SMO algorithm for training
-%       SVMs. In practice, if you want to train an SVM classifier, we
-%       recommend using an optimized package such as:  
+
+% PS: This is a simplified version of the SMO algorithm for training
+%       SVMs. In practice, if you want to train an SVM classifier,
+%       use an optimized package such as:  
 %
 %           LIBSVM   (http://www.csie.ntu.edu.tw/~cjlin/libsvm/)
 %           SVMLight (http://svmlight.joachims.org/)

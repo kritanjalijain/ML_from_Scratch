78fcd175e7b2634a058c5e40e27d644ca51c90f4
Kritanjali Jain <kritanjalijain@gmail.com>
2021-04-17 12:03:59 +0530

    Update svmTrain.m

--- SVM/Spam_Email_Classifier/svmTrain.m
+++ SVM/Spam_Email_Classifier/svmTrain.m
@@ -37,24 +37,20 @@ L = 0;
 H = 0;
 
 % Pre-compute the Kernel Matrix since our dataset is small
-% (in practice, optimized SVM packages that handle large datasets
-%  gracefully will _not_ do this)
 % 
-% We have implemented optimized vectorized version of the Kernels here so
-% that the svm training will run faster.
 if strcmp(func2str(kernelFunction), 'linearKernel')
     % Vectorized computation for the Linear Kernel
-    % This is equivalent to computing the kernel on every pair of examples
+    
     K = X*X';
 elseif strfind(func2str(kernelFunction), 'gaussianKernel')
     % Vectorized RBF Kernel
-    % This is equivalent to computing the kernel on every pair of examples
+    
     X2 = sum(X.^2, 2);
     K = bsxfun(@plus, X2, bsxfun(@plus, X2', - 2 * (X * X')));
     K = kernelFunction(1, 0) .^ K;
 else
     % Pre-compute the Kernel Matrix
-    % The following can be slow due to the lack of vectorization
+    
     K = zeros(m);
     for i = 1:m
         for j = i:m
@@ -78,8 +74,7 @@ while passes < max_passes,
         
         if ((Y(i)*E(i) < -tol && alphas(i) < C) || (Y(i)*E(i) > tol && alphas(i) > 0)),
             
-            % In practice, there are many heuristics one can use to select
-            % the i and j. In this simplified code, we select them randomly.
+           
             j = ceil(m * rand());
             while j == i,  % Make sure i \neq j
                 j = ceil(m * rand());
